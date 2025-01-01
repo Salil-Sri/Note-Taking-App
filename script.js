314486a7b78717109b40da0f56d332a98a678d68
Salil Srivastava <srivastavasalil2211@gmail.com>
2025-01-01 17:31:34 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const NotesContainer = document.querySelector(".notesContainer");
+const button = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showUpdate() {
+  NotesContainer.innerHTML = localStorage.getItem("notes") || "";
+}
+showUpdate() 
+
+function updatestorage() {
+  localStorage.setItem("notes", NotesContainer.innerHTML);
+}
+
+button.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.className = "delete";
+  img.src = "/images/remove-folder-removebg-preview.png";
+  NotesContainer.appendChild(inputBox).appendChild(img);
+});
+
+NotesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updatestorage();
+  } else if (e.target.tagName == "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updatestorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", function(e){
+    if(e.key  === "Enter"){
+        document.execCommand("insertLineBreak");
+        e.preventDefault();
+
+    }
+})
